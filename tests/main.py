b230c25371dcb179402d13f45d92d3a9c68fc480
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2022-04-14 16:31:12 +0300

    Main test

---
+++ tests/main.py
@@ -0,0 +1,4 @@
+from extraneous_activity_delays.bpmn_enhancer import enhance_bpmn_model_with_delays
+
+if __name__ == '__main__':
+    enhance_bpmn_model_with_delays("assets/timer-events-example.bpmn")

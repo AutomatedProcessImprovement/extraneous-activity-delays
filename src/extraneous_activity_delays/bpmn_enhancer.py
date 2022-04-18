bcd4002e4979f9941d2e4aa883858647b2857052
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2022-04-18 15:37:18 +0300

    Refactor BPMN element creation to add namespaces

--- src/extraneous_activity_delays/bpmn_enhancer.py
+++ src/extraneous_activity_delays/bpmn_enhancer.py
@@ -6,7 +6,13 @@ from lxml.etree import QName, ElementTree
 from extraneous_activity_delays.config import DurationDistribution
 
 
-def enhance_bpmn_model_with_delays(document: ElementTree, timers: dict) -> ElementTree:
+def enhance_bpmn_model_with_delays(document: ElementTree, timers: dict):
+    """
+    Enhance the BPMN model received by adding a timer previous to each activity denoted by [timers].
+
+    :param document: XML document containing the BPMN model to enhance.
+    :param timers: dict with the name of each activity as key, and the timer configuration as value.
+    """
     # Extract process and simulation parameters
     model = document.getroot()
     namespace = model.nsmap
@@ -20,39 +26,57 @@ def enhance_bpmn_model_with_delays(document: ElementTree, timers: dict) -> Eleme
             task_id = task.attrib['id']
             # Create a timer to add it preceding to the task
             timer_id = "Event_{}".format(str(uuid.uuid4()))
-            timer = etree.Element("intermediateCatchEvent", {'id': timer_id}, namespace)
+            timer = etree.Element(
+                QName(namespace[None], "intermediateCatchEvent"),
+                {'id': timer_id},
+                namespace
+            )
             # Redirect the edge incoming to the task so it points to the timer
             edge = process.find("sequenceFlow[@targetRef='{}']".format(task_id), namespace)
             edge.attrib['targetRef'] = timer_id
             # Create edge from the timer to the task
             flow_id = "Flow_{}".format(str(uuid.uuid4()))
-            flow = etree.Element("sequenceFlow", {'id': flow_id, 'sourceRef': timer_id, 'targetRef': task_id}, namespace)
+            flow = etree.Element(
+                QName(namespace[None], "sequenceFlow"),
+                {'id': flow_id, 'sourceRef': timer_id, 'targetRef': task_id},
+                namespace
+            )
             # Update incoming flow information inside the task
             task.find("incoming", namespace).text = flow_id
             # Add incoming element inside timer
-            timer_incoming = etree.Element("incoming", {}, namespace)
+            timer_incoming = etree.Element(
+                QName(namespace[None], "incoming"),
+                {},
+                namespace
+            )
             timer_incoming.text = edge.attrib['id']
             timer.append(timer_incoming)
             # Add outgoing element inside timer
-            timer_outgoing = etree.Element("outgoing", {}, namespace)
+            timer_outgoing = etree.Element(
+                QName(namespace[None], "outgoing"),
+                {},
+                namespace
+            )
             timer_outgoing.text = flow_id
             timer.append(timer_outgoing)
             # Timer definition element
             timer_definition_id = "TimerEventDefinition_{}".format(str(uuid.uuid4()))
-            timer_definition = etree.Element("timerEventDefinition", {'id': timer_definition_id}, namespace)
+            timer_definition = etree.Element(
+                QName(namespace[None], "timerEventDefinition"),
+                {'id': timer_definition_id},
+                namespace
+            )
             timer.append(timer_definition)
             # Add the elements to the process
             process.append(timer)
             process.append(flow)
             # Add the simulation config for the timer
             duration_distribution = timers[task_name]
-            sim_timer = get_simulation_timer(timer_id, duration_distribution, namespace)
+            sim_timer = _get_simulation_timer(timer_id, duration_distribution, namespace)
             sim_elements.append(sim_timer)
-    # Return the enhanced document
-    return document
 
 
-def get_simulation_timer(timer_id: str, duration_distribution: DurationDistribution, namespace: dict) -> etree.Element:
+def _get_simulation_timer(timer_id: str, duration_distribution: DurationDistribution, namespace: dict) -> etree.Element:
     sim_timer = etree.Element(QName(namespace['qbp'], "element"), {'elementId': timer_id}, namespace)
     duration_params = {
         'type': duration_distribution.type,

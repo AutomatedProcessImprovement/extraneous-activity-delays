632a1a2c35b8c23656f0e7d18ea00fc1e7886328
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2022-04-18 11:57:01 +0300

    Refactor BPMN model enhancer to add timers based on a dict 'activity name' -> 'timer config'

--- src/extraneous_activity_delays/bpmn_enhancer.py
+++ src/extraneous_activity_delays/bpmn_enhancer.py
@@ -1,15 +1,12 @@
 import uuid
 
 from lxml import etree
-from lxml.etree import QName
+from lxml.etree import QName, ElementTree
 
 from extraneous_activity_delays.config import DurationDistribution
 
 
-def enhance_bpmn_model_with_delays(input_path: str):
-    # Read BPMN model
-    parser = etree.XMLParser(remove_blank_text=True)
-    document = etree.parse(input_path, parser)
+def enhance_bpmn_model_with_delays(document: ElementTree, timers: dict) -> ElementTree:
     # Extract process and simulation parameters
     model = document.getroot()
     namespace = model.nsmap
@@ -17,39 +14,42 @@ def enhance_bpmn_model_with_delays(input_path: str):
     sim_elements = process.find("extensionElements/qbp:processSimulationInfo/qbp:elements", namespace)
     # Add a timer for each task
     for task in process.findall("task", namespace):
-        task_id = task.attrib['id']
-        # Create a timer to add it before the task
-        timer_id = "Event_{}".format(str(uuid.uuid4()))
-        timer = etree.Element("intermediateCatchEvent", {'id': timer_id}, namespace)
-        # Create flow from timer to task
-        flow_id = "Flow_{}".format(str(uuid.uuid4()))
-        flow = etree.Element("sequenceFlow", {'id': flow_id, 'sourceRef': timer_id, 'targetRef': task_id}, namespace)
-        # Update incoming flow information inside the task
-        task.find("incoming", namespace).text = flow_id
-        # Redirect all task-incoming edges to the timer
-        for edge in process.findall("sequenceFlow[@targetRef='{}']".format(task_id), namespace):
+        task_name = task.attrib['name']
+        if task_name in timers:
+            # The activity has a prepared timer -> add it!
+            task_id = task.attrib['id']
+            # Create a timer to add it preceding to the task
+            timer_id = "Event_{}".format(str(uuid.uuid4()))
+            timer = etree.Element("intermediateCatchEvent", {'id': timer_id}, namespace)
+            # Redirect the edge incoming to the task so it points to the timer
+            edge = process.find("sequenceFlow[@targetRef='{}']".format(task_id), namespace)
             edge.attrib['targetRef'] = timer_id
-            # Incoming element inside timer
+            # Create edge from the timer to the task
+            flow_id = "Flow_{}".format(str(uuid.uuid4()))
+            flow = etree.Element("sequenceFlow", {'id': flow_id, 'sourceRef': timer_id, 'targetRef': task_id}, namespace)
+            # Update incoming flow information inside the task
+            task.find("incoming", namespace).text = flow_id
+            # Add incoming element inside timer
             timer_incoming = etree.Element("incoming", {}, namespace)
             timer_incoming.text = edge.attrib['id']
             timer.append(timer_incoming)
-        # Outgoing element inside timer
-        timer_outgoing = etree.Element("outgoing", {}, namespace)
-        timer_outgoing.text = task_id
-        timer.append(timer_outgoing)
-        # Timer definition element
-        timer_definition_id = "TimerEventDefinition_{}".format(str(uuid.uuid4()))
-        timer_definition = etree.Element("timerEventDefinition", {'id': timer_definition_id}, namespace)
-        timer.append(timer_definition)
-        # Add the elements to the process
-        process.append(timer)
-        process.append(flow)
-        # Add the simulation config for the timer
-        duration_distribution = DurationDistribution()  # Default one
-        sim_timer = get_simulation_timer(timer_id, duration_distribution, namespace)
-        sim_elements.append(sim_timer)
-    # Export the enhanced BPMN model
-    document.write("assets/timer-events-example_output.bpmn", pretty_print=True)
+            # Add outgoing element inside timer
+            timer_outgoing = etree.Element("outgoing", {}, namespace)
+            timer_outgoing.text = flow_id
+            timer.append(timer_outgoing)
+            # Timer definition element
+            timer_definition_id = "TimerEventDefinition_{}".format(str(uuid.uuid4()))
+            timer_definition = etree.Element("timerEventDefinition", {'id': timer_definition_id}, namespace)
+            timer.append(timer_definition)
+            # Add the elements to the process
+            process.append(timer)
+            process.append(flow)
+            # Add the simulation config for the timer
+            duration_distribution = timers[task_name]
+            sim_timer = get_simulation_timer(timer_id, duration_distribution, namespace)
+            sim_elements.append(sim_timer)
+    # Return the enhanced document
+    return document
 
 
 def get_simulation_timer(timer_id: str, duration_distribution: DurationDistribution, namespace: dict) -> etree.Element:
--- tests/main.py
+++ tests/main.py
@@ -1,4 +1,20 @@
+from lxml import etree
+
 from extraneous_activity_delays.bpmn_enhancer import enhance_bpmn_model_with_delays
+from extraneous_activity_delays.config import DurationDistribution
 
 if __name__ == '__main__':
-    enhance_bpmn_model_with_delays("assets/timer-events-example.bpmn")
+    # Read BPMN model
+    parser = etree.XMLParser(remove_blank_text=True)
+    document = etree.parse("assets/timer-events-example.bpmn", parser)
+    # Enhance
+    timers = {
+        'Check  application  form completeness': DurationDistribution(mean="60"),
+        'Assess loan risk': DurationDistribution(mean="600"),
+        'Approve application': DurationDistribution(mean="3600"),
+        'Design loan offer': DurationDistribution(mean="7200"),
+        'Approve Loan Offer': DurationDistribution(mean="36000")
+    }
+    enhanced_model = enhance_bpmn_model_with_delays(document, timers)
+    # Export the enhanced BPMN model
+    document.write("assets/timer-events-example_output.bpmn", pretty_print=True)

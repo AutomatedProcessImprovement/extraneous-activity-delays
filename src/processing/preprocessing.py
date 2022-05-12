dabd309beedf42906719527ecba26037c6f38291
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2022-05-12 11:57:49 +0300

    Update synthetic log preprocessing (remove START/END/TIMER's events)

--- src/processing/preprocessing.py
+++ src/processing/preprocessing.py
@@ -43,25 +43,26 @@ def remove_intermediate_event_instances():
     timer_ids = [
         ' EVENT 27 CATCH TIMER', ' EVENT 28 CATCH TIMER', ' EVENT 29 CATCH TIMER', ' EVENT 30 CATCH TIMER', ' EVENT 31 CATCH TIMER',
         ' EVENT 32 CATCH TIMER', ' EVENT 33 CATCH TIMER', ' EVENT 34 CATCH TIMER', ' EVENT 35 CATCH TIMER', ' EVENT 36 CATCH TIMER',
-        ' EVENT 37 CATCH TIMER', ' EVENT 3 START', ' EVENT 27 END'
+        ' EVENT 37 CATCH TIMER', ' EVENT 3 START', ' EVENT 27 END', ' EVENT 18 END ERROR', " Loan application rejected",
+        " Loan  application approved", " Loan  application canceled", " Loan  application received"
     ]
     for log in [
-        "Loan_Application.csv.gz",
-        "Loan_Application_1_timer.csv.gz",
-        "Loan_Application_4_timers.csv.gz",
-        "Pharmacy.csv.gz",
-        "Pharmacy_1_timer.csv.gz",
-        "Pharmacy_4_timers.csv.gz",
-        "Procure_to_Pay.csv.gz",
-        "Procure_to_Pay_1_timer.csv.gz",
-        "Procure_to_Pay_4_timers.csv.gz"
+        "Loan_Application_1_timer_train.csv.gz", "Loan_Application_1_timer_test.csv.gz",
+        "Loan_Application_4_timers_train.csv.gz", "Loan_Application_4_timers_test.csv.gz",
+        "Loan_Application_train.csv.gz", "Loan_Application_test.csv.gz",
+        "Pharmacy_1_timer_train.csv.gz", "Pharmacy_1_timer_test.csv.gz",
+        "Pharmacy_4_timers_train.csv.gz", "Pharmacy_4_timers_test.csv.gz",
+        "Pharmacy_train.csv.gz", "Pharmacy_test.csv.gz",
+        "Procure_to_Pay_1_timer_train.csv.gz", "Procure_to_Pay_1_timer_test.csv.gz",
+        "Procure_to_Pay_4_timers_train.csv.gz", "Procure_to_Pay_4_timers_test.csv.gz",
+        "Procure_to_Pay_train.csv.gz", "Procure_to_Pay_test.csv.gz"
     ]:
         print("\n" + log)
         log_path = "../../inputs/synthetic-simulation-models/" + log
         event_log = pd.read_csv(log_path)
         event_log = event_log[~event_log['Activity'].isin(timer_ids)]
         os.remove(log_path)
-        event_log.to_csv(log_path, encoding="utf-8")
+        event_log.to_csv(log_path, index=False, encoding="utf-8")
 
 
 if __name__ == '__main__':

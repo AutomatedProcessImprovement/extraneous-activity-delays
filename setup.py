3f36a036dfe6a43b8918dcdb7ff196749821cc07
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2022-06-29 15:23:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from setuptools import setup
 
 setup(
     name='extraneous_activity_delays',
-    version='1.0.0',
+    version='1.1.0',
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
         'pandas'
-    ],
-    entry_points={
-        'console_scripts': [
-            'extraneous_activity_delays = main:main',
-        ]
-    }
+        'setuptools'
+        'lxml'
+        'numpy'
+        'scipy'
+        'hyperopt'
+        'log_similarity_metrics'
+    ]
 )

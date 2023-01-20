5b79fcbaba249845b7115a4cbd9463cd8c6e10cd
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-01-20 16:08:30 +0200

    #6 - Implement the option to add timers after each activity

--- src/extraneous_activity_delays/utils/bpmn_enhancement.py
+++ src/extraneous_activity_delays/utils/bpmn_enhancement.py
@@ -9,42 +9,72 @@ from extraneous_activity_delays.config import TimerPlacement
 def add_timer_to_bpmn_model(task, process, namespace, timer_placement: TimerPlacement = TimerPlacement.BEFORE) -> str:
     # The activity has a prepared timer -> add it!
     task_id = task.attrib['id']
-    # Create a timer to add it preceding to the task
+    # Create a timer to add
     timer_id = "Event_{}".format(str(uuid.uuid4()))
     timer = etree.Element(
         QName(namespace[None], "intermediateCatchEvent"),
         {'id': timer_id},
         namespace
     )
-    # Redirect the edge incoming to the task so it points to the timer
-    edge = process.find("sequenceFlow[@targetRef='{}']".format(task_id), namespace)
-    edge.attrib['targetRef'] = timer_id
-    # Create edge from the timer to the task
-    flow_id = "Flow_{}".format(str(uuid.uuid4()))
-    flow = etree.Element(
-        QName(namespace[None], "sequenceFlow"),
-        {'id': flow_id, 'sourceRef': timer_id, 'targetRef': task_id},
-        namespace
-    )
-    # Update incoming flow information inside the task
-    task_incoming = task.find("incoming", namespace)
-    if task_incoming is not None:
-        task_incoming.text = flow_id
-    # Add incoming element inside timer
-    timer_incoming = etree.Element(
-        QName(namespace[None], "incoming"),
-        {},
-        namespace
-    )
-    timer_incoming.text = edge.attrib['id']
+    # Redirect the edge incoming/outgoing to the task, so it points to the timer
+    if timer_placement == TimerPlacement.BEFORE:  # Incoming edge
+        edge = process.find("sequenceFlow[@targetRef='{}']".format(task_id), namespace)
+        edge.attrib['targetRef'] = timer_id
+        # Create edge from the timer to the task
+        flow_id = "Flow_{}".format(str(uuid.uuid4()))
+        flow = etree.Element(
+            QName(namespace[None], "sequenceFlow"),
+            {'id': flow_id, 'sourceRef': timer_id, 'targetRef': task_id},
+            namespace
+        )
+        # Update incoming flow information inside the task
+        task_incoming = task.find("incoming", namespace)
+        if task_incoming is not None:
+            task_incoming.text = flow_id
+        # Add incoming element inside timer
+        timer_incoming = etree.Element(
+            QName(namespace[None], "incoming"),
+            {},
+            namespace
+        )
+        timer_incoming.text = edge.attrib['id']
+        # Add outgoing element inside timer
+        timer_outgoing = etree.Element(
+            QName(namespace[None], "outgoing"),
+            {},
+            namespace
+        )
+        timer_outgoing.text = flow_id
+    else:  # Outgoing edge
+        edge = process.find("sequenceFlow[@sourceRef='{}']".format(task_id), namespace)
+        edge.attrib['sourceRef'] = timer_id
+        # Create edge from the task to the timer
+        flow_id = "Flow_{}".format(str(uuid.uuid4()))
+        flow = etree.Element(
+            QName(namespace[None], "sequenceFlow"),
+            {'id': flow_id, 'sourceRef': task_id, 'targetRef': timer_id},
+            namespace
+        )
+        # Update outgoing flow information inside the task
+        task_outgoing = task.find("outgoing", namespace)
+        if task_outgoing is not None:
+            task_outgoing.text = flow_id
+        # Add outgoing element inside timer
+        timer_outgoing = etree.Element(
+            QName(namespace[None], "outgoing"),
+            {},
+            namespace
+        )
+        timer_outgoing.text = edge.attrib['id']
+        # Add incoming element inside timer
+        timer_incoming = etree.Element(
+            QName(namespace[None], "incoming"),
+            {},
+            namespace
+        )
+        timer_incoming.text = flow_id
+    # Append timer incoming and outgoing info
     timer.append(timer_incoming)
-    # Add outgoing element inside timer
-    timer_outgoing = etree.Element(
-        QName(namespace[None], "outgoing"),
-        {},
-        namespace
-    )
-    timer_outgoing.text = flow_id
     timer.append(timer_outgoing)
     # Timer definition element
     timer_definition_id = "TimerEventDefinition_{}".format(str(uuid.uuid4()))
--- tests/qbp/bpmn_enhancer_test.py
+++ tests/qbp/bpmn_enhancer_test.py
@@ -1,6 +1,6 @@
 from lxml import etree
 
-from extraneous_activity_delays.config import SimulationModel
+from extraneous_activity_delays.config import SimulationModel, TimerPlacement
 from extraneous_activity_delays.qbp.simulation_model_enhancer import add_timers_to_simulation_model
 from extraneous_activity_delays.utils.distributions import DurationDistribution
 
@@ -36,6 +36,71 @@ def test_enhance_bpmn_model_with_delays():
         assert timer.attrib['id'] == timer_to_task.attrib['sourceRef']
         assert timer_to_task.attrib['targetRef'] == task.attrib['id']
         assert timer_to_task.attrib['id'] == task.find("incoming", namespace).text
+        # Simulation parameters asserts
+        sim_timer = sim_elements.find("qbp:element[@elementId='{}']".format(timer.attrib['id']), namespace)
+        duration_distribution = sim_timer.find("qbp:durationDistribution", namespace)
+        time_unit = duration_distribution.find("qbp:timeUnit", namespace)
+        assert time_unit.text == "seconds"
+        if timers[activity].name == "fix":
+            assert duration_distribution.attrib['type'] == "FIXED"
+            assert duration_distribution.attrib['mean'] == str(timers[activity].mean)
+            assert duration_distribution.attrib['arg1'] == "0"
+            assert duration_distribution.attrib['arg2'] == "0"
+        elif timers[activity].name == "norm":
+            assert duration_distribution.attrib['type'] == "NORMAL"
+            assert duration_distribution.attrib['mean'] == str(timers[activity].mean)
+            assert duration_distribution.attrib['arg1'] == str(timers[activity].std)
+            assert duration_distribution.attrib['arg2'] == "0"
+        elif timers[activity].name == "expon":
+            assert duration_distribution.attrib['type'] == "EXPONENTIAL"
+            assert duration_distribution.attrib['mean'] == "0"
+            assert duration_distribution.attrib['arg1'] == str(timers[activity].mean)
+            assert duration_distribution.attrib['arg2'] == "0"
+        elif timers[activity].name == "uniform":
+            assert duration_distribution.attrib['type'] == "UNIFORM"
+            assert duration_distribution.attrib['mean'] == "3600"
+            assert duration_distribution.attrib['arg1'] == str(timers[activity].min)
+            assert duration_distribution.attrib['arg2'] == str(timers[activity].max)
+        elif timers[activity].name == "gamma":
+            assert duration_distribution.attrib['type'] == "GAMMA"
+            assert duration_distribution.attrib['mean'] == str(timers[activity].mean)
+            assert duration_distribution.attrib['arg1'] == str(timers[activity].var)
+            assert duration_distribution.attrib['arg2'] == "0"
+        else:
+            assert False
+
+
+def test_enhance_bpmn_model_with_delays_after():
+    # Read BPMN model
+    parser = etree.XMLParser(remove_blank_text=True)
+    document = etree.parse("./assets/timer-events-test.bpmn", parser)
+    simulation_model = SimulationModel(document)
+    # Enhance
+    timers = {
+        'A': DurationDistribution(name="fix", mean=60, var=0, std=0, min=60, max=60),
+        'B': DurationDistribution(name="norm", mean=1200, var=36, std=6, min=1000, max=1400),
+        'C': DurationDistribution(name="expon", mean=3600, var=100, std=10, min=1200, max=7200),
+        'D': DurationDistribution(name="uniform", mean=3600, var=4000000, std=2000, min=0, max=7200),
+        'E': DurationDistribution(name="gamma", mean=1200, var=144, std=12, min=800, max=1400)
+    }
+    enhanced_simulation_model = add_timers_to_simulation_model(simulation_model, timers, TimerPlacement.AFTER)
+    model = enhanced_simulation_model.bpmn_document.getroot()
+    namespace = model.nsmap
+    process = model.find("process", namespace)
+    sim_elements = process.find("extensionElements/qbp:processSimulationInfo/qbp:elements", namespace)
+    for activity in timers:
+        task = process.find("task[@name='{}']".format(activity), namespace)
+        task_to_timer = process.find("sequenceFlow[@id='{}']".format(task.find("outgoing", namespace).text), namespace)
+        timer = process.find("intermediateCatchEvent[@id='{}']".format(task_to_timer.attrib['targetRef']), namespace)
+        from_timer = process.find("sequenceFlow[@sourceRef='{}']".format(timer.attrib['id']), namespace)
+        # Structural asserts
+        assert from_timer.attrib['sourceRef'] == timer.attrib['id']
+        assert from_timer.attrib['id'] == timer.find("outgoing", namespace).text
+        assert timer.find("incoming", namespace).text == task_to_timer.attrib['id']
+        assert timer.find("timerEventDefinition", namespace).attrib['id'] is not None
+        assert timer.attrib['id'] == task_to_timer.attrib['targetRef']
+        assert task_to_timer.attrib['sourceRef'] == task.attrib['id']
+        assert task_to_timer.attrib['id'] == task.find("outgoing", namespace).text
         # Simulation parameters asserts
         sim_timer = sim_elements.find("qbp:element[@elementId='{}']".format(timer.attrib['id']), namespace)
         duration_distribution = sim_timer.find("qbp:durationDistribution", namespace)

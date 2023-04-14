1114fd32c0359504711f7be9c049561c77945939
David Chapela de la Campa <david.chapela.delacampa@gmail.com>
2023-04-14 15:01:46 +0300

    Rollback ad-hoc modifications to the discovery of delays for InfSys evaluation purposes.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='extraneous_activity_delays',
-    version='2.0.0',
+    version='2.1.0',
     package_dir={"": "src"},
     include_package_data=True,
     install_requires=[
